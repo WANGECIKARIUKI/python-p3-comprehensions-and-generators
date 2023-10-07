de80de4bae227c9983390d9d0b32ca1df4e371fa
LUCY <wangecikariu.luc@gmail.com>
2023-10-07 20:30:47 +0300

    lab done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return_evens = [i for i in num_list if i % 2 == 0]
+    return return_evens
 
 def make_exclamation(sentence_list):
-    pass
+    make_exclamation = [string + "!" for string in sentence_list]
+    return make_exclamation
